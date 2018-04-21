5c6f32bfdb8dd80b2dfe563d9b8068749d76ba1e
Lola Simpson <lolalsimp@gmail.com>
2018-04-21 08:59:37 -0500

    fixed grunt issue

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,37 +1,37 @@
-// let elements = [];
+let elements = [];
 const categories = [];
 
-const getCategories = () => {
-  // return categories;
-  console.log('getCategories', categories);
+const getCategories = (categoriesArray) => {
+  return categories;
+  // console.log('getCategories', categories);
 };
 
-// const setCategories = (categoriesArray) => {
-//   categories = categoriesArray;
-// };
+const setCategories = (categoriesArray) => {
+  categories = categoriesArray;
+};
 
-// const getElements = (elementsArray) => {
-//   return elements;
-// };
+const getElements = (elementsArray) => {
+  return elements;
+};
 
-// const setElements = (elementsArray) => {
-//   elements = elementsArray;
-// };
+const setElements = (elementsArray) => {
+  elements = elementsArray;
+};
 
-// const getElementsByCategories = (categoriesId) => {
-//   const selectedElements = [];
-//   elements.forEach((element) => {
-//     if (element.categoriesId === categoriesId) {
-//       selectedElements.push(element);
-//     }
-//   });
-//   return selectedElements;
-// };
+const getElementsByCategories = (categoriesId) => {
+  const selectedElements = [];
+  elements.forEach((element) => {
+    if (element.categoriesId === categoriesId) {
+      selectedElements.push(element);
+    }
+  });
+  return selectedElements;
+};
 
 module.exports = {
   getCategories,
-  // setCategories,
-  // setElements,
-  // getElements,
-  // getElementsByCategories,
+  setCategories,
+  setElements,
+  getElements,
+  getElementsByCategories,
 };
